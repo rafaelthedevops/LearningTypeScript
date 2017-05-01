dcd655e37c5d663100d14de1a8caa6a81fbfc9c2
Rafael Monteiro <rafaelmonteiroti@gmail.com>
2017-05-01 14:18:45 -0300

    add dependencies

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import * as $ from "jquery";
 import { MustHaveCoffee } from './coffee/getcoffee'
 
 class ReallyCoffee extends MustHaveCoffee {
-    CoffeeType = 'Bulletproof'
+   // coffeeType = 'Bulletproof'
     constructor(){ super() }
     f(input:boolean) {
         let a = 100
@@ -14,14 +14,15 @@ class ReallyCoffee extends MustHaveCoffee {
         return a;
     }
     setCoffeeType(name:string){
-       // super.setCoffeeType(name)
+        super.setCoffeeType(name)
         console.log("hello there " + this.coffeeType)
         $("body").css('background-color', 'red')
+        $("body").html("<h1>" + this.coffeeType + "</h1>")
     }
 }
 
 let newCoffee = new ReallyCoffee()
 newCoffee.f(true)
-newCoffee.setCoffeeType("bulleteproof")
+newCoffee.setCoffeeType("I'm just a Regular coffee")
 
 let oldCoffee = new MustHaveCoffee()
